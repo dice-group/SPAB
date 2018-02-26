f2eea8946fc27a1c88450bad86a46bafc1337cf2
Adrian Wilke <adibaba@users.noreply.github.com>
2018-02-26 18:24:52 +0100

    DBpedia ontology subClassOf test

--- src/main/java/org/aksw/spab/tmp/Dbpedia.java
+++ src/main/java/org/aksw/spab/tmp/Dbpedia.java
@@ -7,11 +7,13 @@ import org.apache.jena.query.Dataset;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ReadWrite;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.tdb.TDBFactory;
+import org.apache.jena.vocabulary.RDFS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,14 +41,20 @@ public class Dbpedia {
 			dbpedia.addModel(model);
 		}
 
-		// Predicate overview
+		// S/P/O overview
+		if (EXECUTE) {
+			dbpedia.printSubjects();
+		}
 		if (EXECUTE) {
 			dbpedia.printAllPredicates();
+		}
+		if (EXECUTE) {
+			dbpedia.printObjects();
 		}
 
 		// Development
 		if (EXECUTE) {
-			dbpedia.test();
+			dbpedia.printSubClassOf();
 		}
 	}
 
@@ -102,7 +110,7 @@ public class Dbpedia {
 		sb.setDistinct(true).addVar("p").addWhere("?s", "?p", "?o").addOrderBy("p");
 		ResultSet results = QueryExecutionFactory.create(sb.build(), TDB).execSelect();
 		while (results.hasNext()) {
-			System.out.println(results.next());
+			System.out.println(results.next().get("p"));
 		}
 		System.out.println();
 
@@ -115,18 +123,10 @@ public class Dbpedia {
 		System.out.println();
 	}
 
-	public void test() {
+	public void printObjects() {
 		SelectBuilder sb;
 		ResultSet results;
-
-		System.out.println("Example subjects: ");
-		sb = new SelectBuilder();
-		sb.setDistinct(true).addVar("s").addWhere("?s", "?p", "?o").addOrderBy("s").setLimit(10);
-		results = QueryExecutionFactory.create(sb.build(), TDB).execSelect();
-		while (results.hasNext()) {
-			System.out.println(results.next());
-		}
-		System.out.println();
+		Query q;
 
 		System.out.println("Example objects: ");
 		sb = new SelectBuilder();
@@ -137,14 +137,6 @@ public class Dbpedia {
 		}
 		System.out.println();
 
-		System.out.println("Number of subjects: ");
-		Query q = QueryFactory.create("SELECT (count(distinct ?s) as ?count) WHERE {?s ?p ?o} GROUP BY ?count");
-		results = QueryExecutionFactory.create(q, TDB).execSelect();
-		while (results.hasNext()) {
-			System.out.println(results.next().get("count").asLiteral().getLexicalForm());
-		}
-		System.out.println();
-
 		System.out.println("Number of objects: ");
 		q = QueryFactory.create("SELECT  (count(distinct ?o) as ?count) WHERE {?s ?p ?o} GROUP BY ?count");
 		results = QueryExecutionFactory.create(q, TDB).execSelect();
@@ -152,6 +144,85 @@ public class Dbpedia {
 			System.out.println(results.next().get("count").asLiteral().getLexicalForm());
 		}
 		System.out.println();
-
 	}
+
+	public void printSubClassOf() {
+		SelectBuilder sb;
+		ResultSet results;
+
+		// <http://dbpedia.org/datatype/fuelType>
+		// <http://dbpedia.org/ontology/Restaurant>
+		String resourceString = "<http://dbpedia.org/ontology/Restaurant>";
+
+		System.out.println("Predicates for " + resourceString + " as object" + " (Limit:10)");
+		sb = new SelectBuilder();
+		sb.setDistinct(true).addVar("p").addWhere("?s", "?p", resourceString).addOrderBy("p").setLimit(10);
+		results = QueryExecutionFactory.create(sb.build(), TDB).execSelect();
+		while (results.hasNext()) {
+			System.out.println(results.next());
+		}
+		System.out.println();
+
+		System.out.println("Predicates for " + resourceString + " as subject" + " (Limit:10)");
+		sb = new SelectBuilder();
+		sb.setDistinct(true).addVar("p").addWhere(resourceString, "?p", "?o").addOrderBy("p").setLimit(10);
+		results = QueryExecutionFactory.create(sb.build(), TDB).execSelect();
+		while (results.hasNext()) {
+			System.out.println(results.next());
+		}
+		System.out.println();
+
+		System.out.println(resourceString + "as Subject" + " (Limit:10)");
+		sb = new SelectBuilder();
+		sb.addWhere(resourceString, "?p", "?o").setLimit(10);
+		results = QueryExecutionFactory.create(sb.build(), TDB).execSelect();
+		while (results.hasNext()) {
+			QuerySolution solution = results.next();
+			System.out.println("<>  " + solution.get("p") + "  " + solution.get("o"));
+		}
+		System.out.println();
+
+		System.out.println(resourceString + "as Object" + " (Limit:10)");
+		sb = new SelectBuilder();
+		sb.addWhere("?s", "?p", resourceString).setLimit(10);
+		results = QueryExecutionFactory.create(sb.build(), TDB).execSelect();
+		while (results.hasNext()) {
+			QuerySolution solution = results.next();
+			System.out.println(solution.get("s") + "  " + solution.get("p") + "  <>");
+		}
+		System.out.println();
+
+		System.out.println("<> subClassOf ?o" + " (Limit:10)");
+		sb = new SelectBuilder();
+		sb.addWhere(resourceString, RDFS.subClassOf, "?o").setLimit(10);
+		results = QueryExecutionFactory.create(sb.build(), TDB).execSelect();
+		while (results.hasNext()) {
+			System.out.println(results.next());
+		}
+		System.out.println();
+	}
+
+	public void printSubjects() {
+		SelectBuilder sb;
+		ResultSet results;
+		Query q;
+
+		System.out.println("Example subjects: ");
+		sb = new SelectBuilder();
+		sb.setDistinct(true).addVar("s").addWhere("?s", "?p", "?o").addOrderBy("s").setLimit(10);
+		results = QueryExecutionFactory.create(sb.build(), TDB).execSelect();
+		while (results.hasNext()) {
+			System.out.println(results.next());
+		}
+		System.out.println();
+
+		System.out.println("Number of subjects: ");
+		q = QueryFactory.create("SELECT (count(distinct ?s) as ?count) WHERE {?s ?p ?o} GROUP BY ?count");
+		results = QueryExecutionFactory.create(q, TDB).execSelect();
+		while (results.hasNext()) {
+			System.out.println(results.next().get("count").asLiteral().getLexicalForm());
+		}
+		System.out.println();
+	}
+
 }
